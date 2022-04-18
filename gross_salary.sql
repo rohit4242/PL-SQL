7f72d1cc8532e12b3b78eebd32116ecfecc71232
Rohit Luni <87267331+rohit4242@users.noreply.github.com>
2022-04-18 10:34:20 +0530

    Add files via upload

---
+++ gross_salary.sql
@@ -0,0 +1,27 @@
+declare 
+    basic_salary emp10.basicsal%type;
+    employee_id emp10.eid%type:=1;
+    employee_name emp10.ename%type;
+
+    DA number(8,2):=0;
+    HRA number(8,2):=0;
+    MADICAL number(8,2):=0;
+    PF number(8,2):=0;
+
+    gross_salary number(8,2):=0;
+
+
+begin
+    select eid,ename,basicsal into employee_id,employee_name,basic_salary from emp10 where eid=1;
+
+    HRA:=(basic_salary*15)/100;
+    DA:=(basic_salary*50)/100;
+    
+    PF:=(basic_salary*10)/100;
+
+    gross_salary:=basic_salary+(DA+HRA+MADICAL)-PF;
+
+    dbms_output.put_line('Your gross salary is: '|| gross_salary);
+
+end;
+/

f618d2ca2d4a1829c4009dc159591160464714d9
ROHIT LUNI <rohitluni123@gmail.com>
2022-04-26 15:27:10 +0530

    init

--- query.sql
+++ query.sql
@@ -1,16 +1,19 @@
--- write a pl/sql block that uses a cursor attribute sql%rowcount to raise the basic salary of employees by 10% that are working in department number 10 and also display the apporpriate message based on the existence to the record in the emp table (use implicit cursor).
+-- write a pl/sql block that uses a cursor attribute %isopen and %notfound to
+-- raise the basic salary of employees of department number 20 by 5% 
+-- and also display the apporpriate message based no the existence to the record in the emp table.
+-- whenever any such raise is given to the employee, a record for the same is mainttained in the emp_update table.
+-- (perform using both implicit and explicit cursors)
 
 declare 
- 
+
 begin
-    update customer_rec set salary = salary +(salary * (0.1)) where dept_no = 10;
-    if sql%found then
-       
+
+    update customer_rec set salary = salary * 1.1 where dept_no = 10;
+    if sql%isopen then 
         dbms_output.put_line('The salary of the employee is increased by 10%');
     elsif sql%notfound then
         dbms_output.put_line('The employee does not exist');
-     update customer_rec set salary = salary +(salary * (0.1)) where dept_no = 10;
     end if;
+
 end;
 /
-

dbee1d0d52a897dba83ec8062a5f889dcfece961
ROHIT LUNI <rohitluni123@gmail.com>
2022-05-02 10:26:02 +0530

    init

--- unit_2/program_4.sql
+++ unit_2/program_4.sql
@@ -4,22 +4,20 @@
 
 declare
     cursor c1 is select * from customer_rec order by salary desc;
-    i number(2):=0;
+    i number(2):= 0;
     r_row customer_rec%rowtype;
 
 begin
     open c1;
     loop
         fetch c1 into r_row;
-        
-        exit when i = 3;
-        i := i + 1;
-
+        exit when c1%rowcount = 6;
+       
         dbms_output.put_line( r_row.id || ' ' ||r_row.name || ' ' || r_row.salary);
-        dbms_output.put_line('Rowcount: ' || c1%rowcount);
 
     end loop;
-
+        dbms_output.put_line('Rowcount: ' || c1%rowcount);
+        
     close c1;
 end;
 /

b8c181a5e084d4864e642a4bc434bf6aacc8c89c
ROHIT LUNI <rohitluni123@gmail.com>
2022-05-02 09:39:42 +0530

    init

--- unit_1/program_7.sql
+++ unit_1/program_7.sql
@@ -4,3 +4,54 @@
 -- with Rollno, Name, Sub1, Sub2, Sub3, Sub4, Sub5,
 -- Total, Per, Grade attributes with appropriate data 
 -- type).
+
+Create Table RESULT(
+    Rollno Number(10),
+    Name Varchar2(20),
+    Sub1 Number(10),
+    Sub2 Number(10),
+    Sub3 Number(10),
+    Sub4 Number(10),
+    Sub5 Number(10),
+    Total Number(10),
+    Per Number(10),
+    Grade Varchar2(10)
+);
+
+insert into RESULT values(1,'A',100,100,100,100,100,500,500,'A');
+insert into RESULT values(2,'B',100,100,100,100,100,500,500,'A');
+insert into RESULT values(3,'C',100,100,100,100,100,500,500,'A');
+insert into RESULT values(4,'D',100,100,100,100,100,500,500,'A');
+insert into RESULT values(5,'E',100,100,100,100,100,500,500,'A');
+insert into RESULT values(6,'F',100,100,100,100,100,500,500,'A');
+insert into RESULT values(7,'G',100,100,100,100,100,500,500,'A');
+insert into RESULT values(8,'H',100,100,100,100,100,500,500,'A');
+insert into RESULT values(9,'I',100,100,100,100,100,500,500,'A');
+insert into RESULT values(10,'J',100,100,100,100,100,500,500,'A');
+
+declare 
+    cnt number;
+    total number;
+    per number;
+    grade varchar2(10);
+begin
+    select count(*) into cnt from RESULT;
+    select sum(Total) into total from RESULT;
+    select sum(Per) into per from RESULT;
+    if per >= 90 then
+        grade := 'A';
+    elsif per >= 80 then
+        grade := 'B';
+    elsif per >= 70 then
+        grade := 'C';
+    elsif per >= 60 then
+        grade := 'D';
+    else
+        grade := 'F';
+    end if;
+    dbms_output.put_line('Total Students: ' || cnt);
+    dbms_output.put_line('Total Marks: ' || total);
+    dbms_output.put_line('Total Percentage: ' || per);
+    dbms_output.put_line('Grade: ' || grade);
+end;
+/
--- unit_2/program_3.sql
+++ unit_2/program_3.sql
@@ -2,3 +2,25 @@
 -- %ROWCOUNT to display the name, department 
 -- and basic salary of first 5 employees getting the 
 -- highest basic salary. (Use Explicit Cursor)
+
+declare
+    cursor c1 is select * from customer_rec order by salary desc;
+    name varchar2(20);
+    basic_salary number(10);
+    i number(2):=0;
+    r_row customer_rec%rowtype;
+
+begin
+    open c1;
+    loop
+        fetch c1 into r_row;
+        exit when i = 5;
+        i := i + 1;
+        name := r_row.name;
+        basic_salary := r_row.salary;
+        dbms_output.put_line(name || ' ' || basic_salary);
+    end loop;
+
+    close c1;
+end;
+/

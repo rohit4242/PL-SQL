de78e60374231ff7753424d23b4d233a49dc90bf
ROHIT LUNI <rohitluni123@gmail.com>
2022-05-02 10:02:46 +0530

    init

--- unit_2/program_4.sql
+++ unit_2/program_4.sql
@@ -1,3 +1,22 @@
 -- 4) Write a PL/SQL block using a cursor FOR loop to 
 -- display name and the basic salary of top 3 highest 
 -- paid employees. (Use Cursor For Loop)
+
+declare
+    cursor c1 is select * from customer_rec order by salary desc;
+    i number(2):=0;
+    r_row customer_rec%rowtype;
+
+begin
+    open c1;
+    loop
+        fetch c1 into r_row;
+        exit when i = 3;
+        i := i + 1;
+
+        dbms_output.put_line( r_row.id || ' ' ||r_row.name || ' ' || r_row.salary);
+    end loop;
+
+    close c1;
+end;
+/

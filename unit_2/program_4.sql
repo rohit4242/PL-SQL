25a538fdc13d4c61b8ead21456f813a3e429e6f9
ROHIT LUNI <rohitluni123@gmail.com>
2022-05-02 10:41:55 +0530

    init

--- unit_2/program_4.sql
+++ unit_2/program_4.sql
@@ -2,22 +2,26 @@
 -- display name and the basic salary of top 3 highest 
 -- paid employees. (Use Cursor For Loop)
 
+
 declare
     cursor c1 is select * from customer_rec order by salary desc;
-    i number(2):= 0;
     r_row customer_rec%rowtype;
+    num number(2):=0;
+    row_count number(2):=0;
 
 begin
     open c1;
     loop
+        num:=&num;
+        row_count := c1%rowcount;
         fetch c1 into r_row;
-        exit when c1%rowcount = 6;
+        exit when row_count = num;
        
         dbms_output.put_line( r_row.id || ' ' ||r_row.name || ' ' || r_row.salary);
-
-    end loop;
-        dbms_output.put_line('Rowcount: ' || c1%rowcount);
-        
+    
+    end loop;  
+    
+    dbms_output.put_line('Rowcount: ' || row_count);
     close c1;
 end;
 /

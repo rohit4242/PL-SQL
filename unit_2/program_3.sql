07d37814f0c3ff1373cc058d9aafa2bd681b9580
ROHIT LUNI <rohitluni123@gmail.com>
2022-05-02 10:01:35 +0530

    init

--- unit_2/program_3.sql
+++ unit_2/program_3.sql
@@ -5,8 +5,6 @@
 
 declare
     cursor c1 is select * from customer_rec order by salary desc;
-    name varchar2(20);
-    basic_salary number(10);
     i number(2):=0;
     r_row customer_rec%rowtype;
 
@@ -16,9 +14,8 @@ begin
         fetch c1 into r_row;
         exit when i = 5;
         i := i + 1;
-        name := r_row.name;
-        basic_salary := r_row.salary;
-        dbms_output.put_line(name || ' ' || basic_salary);
+
+        dbms_output.put_line( r_row.id || ' ' ||r_row.name || ' ' || r_row.salary);
     end loop;
 
     close c1;

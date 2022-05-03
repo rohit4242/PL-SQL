84308f610151d9ae89b1aede761e3acb9d3a4127
ROHIT LUNI <rohitluni123@gmail.com>
2022-05-03 15:17:51 +0530

    init

--- query.sql
+++ query.sql
@@ -11,17 +11,20 @@ insert into products values(3,'popcorn',50.00);
 insert into products values(4,'sandwich',150.00);
 
 declare
-    cursor cr_pro is select * from products order by list_price desc;
-    row_count number(2):=0;
-    num number(2):=0;
-begin
+	cursor c_pro (low_price number, high_price number) is select * from products where list_price between low_price and high_price;
+    r_row products%rowtype;
+    low_price number(8,2):=0;
+    high_price number(8,2):=0;
 
-    for r_row in cr_pro loop
-        num:=&num;
-        row_count := cr_pro%rowcount;
-        dbms_output.put_line( r_row.product_id || ' ' ||r_row.product_name || ' ' || r_row.list_price);
-        exit when row_count = num;
+begin
+    low_price:=&low_price;
+    high_price:=&high_price;
+    open c_pro(low_price,high_price);
+    loop
+        fetch c_pro into r_row;
+        exit when c_pro%notfound;
+        dbms_output.put_line(r_row.product_id||' '||r_row.product_name||' '||r_row.list_price);
     end loop;
-    dbms_output.put_line('Rowcount: ' || row_count);
+    close c_pro;
 end;
 /
--- unit_1/program_9.sql
+++ unit_1/program_9.sql
@@ -1,3 +1,14 @@
 -- 9) Write a PL/SQL block which displays all records of 
 -- Male employees working in HR Dept from EMP 
 -- table.
+
+declare
+    cursor c1 is select * from emp10 where G='m';
+
+begin
+    for i in c1
+    loop 
+        dbms_output.put_line(i.eid||' '||i.name);
+    end loop;
+end;
+/

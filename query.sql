af142c0bde7fb809355429a39f77e6b594c7bc77
ROHIT LUNI <rohitluni123@gmail.com>
2022-05-03 14:17:07 +0530

    init

--- query.sql
+++ query.sql
@@ -1,19 +1,27 @@
--- write a pl/sql block that uses a cursor attribute %isopen and %notfound to
--- raise the basic salary of employees of department number 20 by 5% 
--- and also display the apporpriate message based no the existence to the record in the emp table.
--- whenever any such raise is given to the employee, a record for the same is mainttained in the emp_update table.
--- (perform using both implicit and explicit cursors)
+create table products(
+    product_id number(4) primary key,
+    product_name varchar2(50),
+    list_price number(8,2)
+);
 
-declare 
 
+insert into products values(1,'cake',100.00); 
+insert into products values(2,'colddrink',20.00);
+insert into products values(3,'popcorn',50.00);
+insert into products values(4,'sandwich',150.00);
+
+declare
+    cursor cr_pro is select * from products order by list_price desc;
+    row_count number(2):=0;
+    num number(2):=0;
 begin
 
-    update customer_rec set salary = salary * 1.1 where dept_no = 10;
-    if sql%isopen then 
-        dbms_output.put_line('The salary of the employee is increased by 10%');
-    elsif sql%notfound then
-        dbms_output.put_line('The employee does not exist');
-    end if;
-
+    for r_row in cr_pro loop
+        num:=&num;
+        row_count := cr_pro%rowcount;
+        dbms_output.put_line( r_row.product_id || ' ' ||r_row.product_name || ' ' || r_row.list_price);
+        exit when row_count = num;
+    end loop;
+    dbms_output.put_line('Rowcount: ' || row_count);
 end;
 /

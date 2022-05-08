eaea6847e9282ffb9195186169e34a24229ed9c1
rohit4242 <rohitluni4242@gmail.com>
2022-05-08 15:16:38 +0530

    init

--- unit_1/program_10.sql
+++ unit_1/program_10.sql
@@ -1,3 +1,13 @@
 -- 10) Write a PL/SQL block to delete the record of 
 -- employee for given EID.
 
+declare
+    eid number;
+begin
+    
+    select eid into eid from employee;
+    delete from employee where eid = eid;
+    
+end;
+/
+

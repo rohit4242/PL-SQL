13afc5f6bdb502ebba61794ceb11fa1cffb1a522
ROHIT LUNI <rohitluni123@gmail.com>
2022-06-02 11:05:00 +0530

    init

--- query.sql
+++ query.sql
@@ -1,40 +1,52 @@
-create table products(
-    product_id number(4) primary key,
-    product_name varchar2(50),
-    list_price number(8,2)
-);
+-- create table products(
+--     product_id number(4) primary key,
+--     product_name varchar2(50),
+--     list_price number(8,2)
+-- );
 
 
-insert into products values(1,'cake',100.00); 
-insert into products values(2,'colddrink',20.00);
-insert into products values(3,'popcorn',50.00);
-insert into products values(4,'sandwich',150.00);
+-- insert into products values(1,'cake',100.00); 
+-- insert into products values(2,'colddrink',20.00);
+-- insert into products values(3,'popcorn',50.00);
+-- insert into products values(4,'sandwich',150.00);
 
-declare
-	cursor c_pro (low_price number, high_price number) is select * from products where list_price between low_price and high_price;
-    r_row products%rowtype;
-    low_price number(8,2):=0;
-    high_price number(8,2):=0;
+-- declare
+-- 	cursor c_pro (low_price number, high_price number) is select * from products where list_price between low_price and high_price;
+--     r_row products%rowtype;
+--     low_price number(8,2):=0;
+--     high_price number(8,2):=0;
+
+-- begin
+--     low_price:=&low_price;
+--     high_price:=&high_price;
+--     open c_pro(low_price,high_price);
+--     loop
+--         fetch c_pro into r_row;
+--         exit when c_pro%notfound;
+--         dbms_output.put_line(r_row.product_id||' '||r_row.product_name||' '||r_row.list_price);
+--     end loop;
+--     close c_pro;
+--     declare
+--     temp varchar2(20);
+
+-- exception when too_many_rows then
+--     dbms_output.put_line('error trying to select too many rows');
+-- exception when no_data_found then
+--     dbms_output.put_line('No data found');
+-- exception when others then
+--     dbms_output.put_line('some runtime error');
+
+-- end;
+-- /
+--  start F:\Rohit\PL-SQL-4\query.sql
+
+create or replace procedure hello_msg(name in varchar2,nm in varchar2)
+is
 
 begin
-    low_price:=&low_price;
-    high_price:=&high_price;
-    open c_pro(low_price,high_price);
-    loop
-        fetch c_pro into r_row;
-        exit when c_pro%notfound;
-        dbms_output.put_line(r_row.product_id||' '||r_row.product_name||' '||r_row.list_price);
-    end loop;
-    close c_pro;
-    declare
-    temp varchar2(20);
+    dbms_output.put_line('Hello '||name||'!'|| nm);
+    
 
-exception when too_many_rows then
-    dbms_output.put_line('error trying to select too many rows');
-exception when no_data_found then
-    dbms_output.put_line('No data found');
-exception when others then
-    dbms_output.put_line('some runtime error');
-
-end;
+    end;
 /
+exec hello_msg('Rohit',45);
